e1eeffb83e090a1b57f23b74a57a21420fabe113
Rasmus Haglund <rasmus.haglund@gmail.com>
2018-04-28 08:31:50 +0200

    Added additional security for admin

--- partisk/views/admin.py
+++ partisk/views/admin.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
 from partisk.models import Stuff, Question, Answer
-from partisk.forms import QuestionModelForm, StuffModelForm, AnswerModelForm
+from partisk.forms import QuestionModelForm, StuffModelForm, AnswerModelForm, ApproveAnswerModelForm
 from django.forms import ModelForm
 from django.forms.models import inlineformset_factory
 from django.shortcuts import render, get_object_or_404, redirect
@@ -14,16 +14,19 @@ def login(request):
     pass
 
 
+@login_required
 def logout(request):
     pass
 
 
+@login_required
 def handle(request):
     obj = Stuff.objects.filter(handled=False).first()
     return redirect(reverse('stuff_detail',
                     kwargs={'pk': obj.id}))
 
 
+@login_required
 def stuff(request):
     stuff = Stuff.objects.filter(handled=False).order_by('-date', 'id')
     context = {
@@ -33,6 +36,7 @@ def stuff(request):
     return render(request, 'admin/stuff.html', context)
 
 
+@login_required
 class StuffDetail(DetailView):
     model = Stuff
     template_name = 'admin/stuff_detail.html'
@@ -59,6 +63,24 @@ class StuffDetail(DetailView):
         return context
 
 
+@login_required
+def approve_answers(request):
+    obj = Answer.objects.filter(approved=False).first()
+    return redirect(reverse('approve_answer_detail',
+                    kwargs={'pk': obj.id}))
+
+
+@login_required
+class ApproveAnswerDetail(UpdateView):
+    model = Answer
+    template_name = 'admin/approve_answer_detail.html'
+    form_class = ApproveAnswerModelForm
+
+    def get_success_url(self):
+        return reverse('approve_answers')
+
+
+@login_required
 @permission_required('partisk.edit_stuff')
 def stuff_done(request, stuff_id):
     stuff = get_object_or_404(Stuff, id=stuff_id)
@@ -68,5 +90,6 @@ def stuff_done(request, stuff_id):
     return redirect('handle')
 
 
+@login_required
 def admin_index(request):
     return render(request, 'admin/admin.html')
