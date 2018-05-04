fd6b158cadd0a29ad7fae0578f095f3938e1242f
Rasmus Haglund <rasmus.haglund@gmail.com>
2018-05-04 22:10:49 +0200

    Preparing for docker deploy

---
+++ app/settings/prod.py
@@ -0,0 +1,49 @@
+import os
+from app.settings.common import *
+
+MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'pipeline.middleware.MinifyHTMLMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware'
+]
+
+DEBUG = False
+COMPRESS_ENABLED = True
+
+VIEW_CACHE_TIME = 0
+CACHE_MIDDLEWARE_SECONDS = VIEW_CACHE_TIME
+
+CACHES = {
+    'default':  {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': VIEW_CACHE_TIME,
+    },
+    'images': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'OPTIONS': {'MAX_ENTRIES': 2},
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'partisk',
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT', '3306')
+    }
+}
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+ADMIN_ENABLED = os.environ['ADMIN_ENABLED'] is True
