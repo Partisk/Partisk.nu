80a0365521080249f9523181ba17c08d1afe65a2
Rasmus Haglund <rasmus.haglund@gmail.com>
2018-05-09 22:18:49 +0200

    Model tests

--- partisk/tests/test_models.py
+++ partisk/tests/test_models.py
@@ -1,12 +1,24 @@
 # https://realpython.com/testing-in-django-part-1-best-practices-and-examples/
 
 from django.test import TestCase
-from partisk.models import Question
+from partisk.models import Question, Party, Tag, Quiz
 
 
-class TestQuestions(TestCase):
+class TestSlugs(TestCase):
     fixtures = ['partisk.yaml', ]
 
-    def test_get_question(self):
-            q = Question.objects.get(pk=1)
-            self.assertEquals(q.title, 'Question 1')
+    def test_question_slug(self):
+        q = Question.objects.get(pk=1)
+        self.assertEquals(q.slug, 'question_1')
+
+    def test_party_slug(self):
+        p = Party.objects.get(pk=1)
+        self.assertEquals(p.slug, 'party_1')
+
+    def test_quiz_slug(self):
+        q = Quiz.objects.get(pk=1)
+        self.assertEquals(q.slug, 'quiz_1')
+
+    def test_tag_slug(self):
+        t = Tag.objects.get(pk=1)
+        self.assertEquals(t.slug, 'tag_1')
